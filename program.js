0442218394edce1eb2c83bae5a757ea307b934b3
claire froelich <claire.froelich@gmail.com>
2020-06-26 19:30:59 -0400

    refactor, rm enquirer

--- program.js
+++ program.js
@@ -6,10 +6,7 @@ const shell = require('shelljs');
 
 const enquirer  = require('./lib/enquirer');
 const files = require('./lib/files');
-const react = require('./lib/react');
-const message = require('./lib/message');
 const create = require('./lib/create');
-const TemplatingService = require('./service/templating.service');
 const { version } = require("./package.json");
 
 const createProgram = () => {
@@ -19,27 +16,12 @@ const createProgram = () => {
   program.version(version);
 
   program
-    .command('new <project>')
-    .alias('n')
-    .description('create new project directory of type -t')
-    .option('-t, --type <type>','specify project type: react, js', 'react')
-    .action(function (project, cmdObj) {
-      message.banner();
-
-      const templatingService = new TemplatingService();
-      templatingService.template('vanilla', { projectName: project });
-
-      // case: react
-      // if(cmdObj.type ==='react') {
-      //   console.log(chalk.green(`Starting new react project`),
-      //   chalk.bold.blue(`'${project}'`),
-      //   chalk.green(`...`)
-      // );
-      //   enquirer.askGithubUsername()
-      //     .then(res => react.createReactApp(project, res.username))
-      //     .catch(err => console.log(chalk.red(err)))
-      // }
-    });
+    .command('create [project]')
+    .alias('c')
+    .description('start new project from template')
+    .action(function (project) {
+      create.newProject(project)
+    })
 
   program
     .command('deploy')
@@ -50,21 +32,12 @@ const createProgram = () => {
       console.log(chalk.green(`Done! 'https://${username}.io/${path.basename(CWD)}/'`))
     })
 
-  program
-    .command('create [project]')
-    .alias('c')
-    .description('start new project from template')
-    .action(function (project) {
-      create.newProject(project)
-    })
-
   // future commands
   program
     .command('config')
     .description('(future command) set up mintbean credentials etc.')
     .action(function (cmdObj) {
       console.log('config-ing....')
-      // console.log(cmdObj )
     });
 
   return program;
@@ -73,24 +46,3 @@ const createProgram = () => {
 module.exports = {
   createProgram
 };
-
-//
-// if (!files.directoryIsEmpty('.')) {
-//   console.log(chalk.red('Error: Project of name  already exists. Run \'mintbean -i\ <project-name>\' in empty project folder'))
-//   process.exit();
-// };
-//
-// const run = async () => {
-//   const credentials = await enquirer.askGithubCredentials();
-//   console.log(credentials);
-// };
-
-// const argv = require('minimist')(process.argv)
-// console.log(argv)
-// const run = async () => {
-//   shell.exec('git init')
-//
-//   shell.exec('hub create')
-// };
-
-// run();
