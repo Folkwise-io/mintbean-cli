fe008aaac07b0429ea88d94e38f666166b4b5ffe
claire froelich <claire.froelich@gmail.com>
2020-06-26 23:54:48 -0400

    rm test folders

--- van1/src/index.js
+++
@@ -1,7 +0,0 @@
-const output = document.getElementById('output');
-const incrementButton = document.getElementById('increment-button')
-
-incrementButton.addEventListener('click', () => {
-  const currentNumber = Number.parseInt(output.innerText);
-  output.innerText = currentNumber + 1;
-});
