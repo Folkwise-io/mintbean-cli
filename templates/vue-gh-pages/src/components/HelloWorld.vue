d27e8ead9dfe99046040227f6e6389acbf82598f
claire froelich <claire.froelich@gmail.com>
2020-07-01 18:35:45 -0400

    add subtitle

--- templates/vue-gh-pages/src/components/HelloWorld.vue
+++ templates/vue-gh-pages/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>HAPPY HACKING!</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
