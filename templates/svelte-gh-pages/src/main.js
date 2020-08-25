e89c1496869d204638b57e18aad28b00b5b9eabb
kmbarnett <kmbarnett94@gmail.com>
2020-08-25 11:13:27 -0600

    Add Right Files

--- templates/svelte-gh-pages/src/main.js
+++ templates/svelte-gh-pages/src/main.js
@@ -1,9 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	// Target is where the app will be injected
 	target: document.body,
-	// props is anything I want to pass to the props
+	props: {
+		name: 'world'
+	}
 });
 
 export default app;
