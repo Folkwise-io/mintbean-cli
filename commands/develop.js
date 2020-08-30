c4bde633654fb1da3ec4774342f68d4662bd840d
kmbarnett <kmbarnett94@gmail.com>
2020-08-30 08:56:40 -0600

    Burn files replaced with direct commands or pkgs

--- commands/develop.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-const shell = require('shelljs');
-const files = require('../lib/files');
-
-module.exports = {
-  develop: () => {
-    shell.exec('npm run start');
-  }
-}
