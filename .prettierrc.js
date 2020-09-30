bfdbaeb2bd5ae411318aced9607c7c56ca4b2202
kmbarnett <kmbarnett94@gmail.com>
2020-09-30 13:21:11 -0600

    Add format config

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+// {
+//   "printWidth": 80,
+//   "tabWidth": 2,
+//   "singleQuote": true,
+//   "trailingComma": "es5",
+//   "semi": true,
+//   "newline-before-return": true,
+//   "no-duplicate-variable": [true, "check-parameters"],
+//   "no-var-keyword": true
+// }
+
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 120,
+  tabWidth: 2,
+};
