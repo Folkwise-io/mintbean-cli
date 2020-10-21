f9b452c5b34570c8db847906005dc3acd6bd109d
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-10-21 11:58:05 -0400

    add mint lint command, add eslint to mintbean-cli, add eslintrc basic config file to each template

--- commands/lint.js
+++ commands/lint.js
@@ -2,19 +2,17 @@ import chalk from 'chalk';
 import cp from 'child_process';
 
 export const lint = (lintSources = 'src/') => {
-  console.log(chalk.cyanBright('Linting...'));
+  const path = __dirname + '/../node_modules/eslint/bin/eslint.js'
 
-  const eslint = cp.spawn('eslint', [lintSources], {
+  const eslint = cp.spawn(path, [lintSources], {
     stdio: ['inherit', 'inherit', 'pipe']
   });
 
   eslint.on('error', (error) => {
       if (error.errno === -2) {
-        // Better ENOENT error message
-        console.log(chalk.red('error: Directory "src" not found: "mint lint" should be ran from the project\'s root directory'))
-      } else {
         console.log(chalk.red(`error: ${error.message}`));
       }
   });
 };
+
 
